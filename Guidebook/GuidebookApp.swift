1bc947839bbff2e8d364429c3e27635ff9244466
Jayanta Adhikary <adhikaryjayanta@outlook.com>
2023-07-28 21:47:16 +0530

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Jayanta Adhikary on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
